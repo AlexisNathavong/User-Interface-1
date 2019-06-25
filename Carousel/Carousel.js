e15b1839b05064fcb06243f0e2c09a2e91d93217
Alexis Panyathong <alexispanyathong@gmail.com>
2019-06-25 16:16:20 -0500

    Making progress...
    I had to add the doc tag that contained the metaport tags and everything else becuase I didn't have one to start with. I managed to get most of the carousel to work and changed the carousel width and height to fit the page better.

--- Carousel/Carousel.js
+++ Carousel/Carousel.js
@@ -3,11 +3,26 @@ class Carousel {
         this.element = element;
         this.LeftArrow = this.element.querySelector('.left-button');
         this.RightArrow = this.element.querySelector('.right-button');
-        this.imgs = document.querySelectorAll('.carousel img');
+        this.imgs = document.querySelectorAll('.carousel-caption');
         this.index = 0;
-        this.imgs[0].style.display = "block";
-        
+        this.imgs[this.index].style.display = "block";
+        this.RightArrow.addEventListener("click", () => this.moveright());
+        this.LeftArrow.addEventListener("click", () => this.moveleft());
+    }
+
+    moveright() {
+        this.imgs[this.index].style.display = "none";
+        this.index += 1;
+        this.imgs[this.index].style.display = "block";
+
+    }
+
+    moveleft() {
+        this.imgs[this.index].style.display = "none";
+        this.index -=1;
+        this.imgs[this.index].style.display = "block";
     }
 }
 
 let carousel = new Carousel(document.querySelector('.carousel'));
+console.log(carousel.imgs);
