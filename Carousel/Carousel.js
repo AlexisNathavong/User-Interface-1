6a9316033a0dba09f4dd523119143e37e01ce80f
Alexis Panyathong <alexispanyathong@gmail.com>
2019-06-26 11:33:43 -0500

    Finally figured out the carousel...
    I was able to create an infinite loop to my carousel where it goes back to the first image after it reaches the end.

--- Carousel/Carousel.js
+++ Carousel/Carousel.js
@@ -8,11 +8,20 @@ class Carousel {
         this.imgs[this.index].style.display = "block";
         this.RightArrow.addEventListener("click", () => this.moveright());
         this.LeftArrow.addEventListener("click", () => this.moveleft());
+        
     }
 
     moveright() {
         this.imgs[this.index].style.display = "none";
-        this.index += 1;
+       
+        if (this.imgs.length === this.index+1) {
+            this.index = 0;
+
+        }
+        else {
+            this.index += 1;
+ 
+        }        
         this.imgs[this.index].style.display = "block";
 
     }
